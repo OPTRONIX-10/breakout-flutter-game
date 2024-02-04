6a66c0aca9c7aa188875f9fd7210a887343d2dac
Abhishek <ksabhishek00@gmail.com>
2024-02-04 15:38:38 +0530

    game completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:flame/game.dart';
-import 'package:flame_game_1/src/brick_breaker.dart';
+import 'package:flame_game_1/src/widgets/game_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  final game = BrickBreaker();
-  runApp(GameWidget(game: game));
+  runApp(const GameApp());
 }
--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/extensions.dart';
 import 'package:flame/game.dart';
 import 'package:flame_game_1/src/components/components.dart';
@@ -9,7 +10,13 @@ import 'package:flame_game_1/src/components/play_area.dart';
 import 'package:flame_game_1/src/config.dart';
 import 'dart:math' as math;
 
-class BrickBreaker extends FlameGame with HasCollisionDetection{
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+enum PlayState { welcome, playing, gameOver, won }
+
+class BrickBreaker extends FlameGame
+    with HasCollisionDetection, KeyboardEvents, TapDetector {
   BrickBreaker()
       : super(
           camera: CameraComponent.withFixedResolution(
@@ -20,19 +27,87 @@ class BrickBreaker extends FlameGame with HasCollisionDetection{
   double get width => size.x;
   double get height => size.y;
 
-  @override
-  FutureOr<void> onLoad() {
+  late PlayState _playState;
+  PlayState get playState => _playState;
+  set playState(PlayState playState) {
+    _playState = playState;
+    switch (playState) {
+      case PlayState.welcome:
+      case PlayState.gameOver:
+      case PlayState.won:
+        overlays.add(playState.name);
+      case PlayState.playing:
+        overlays.remove(PlayState.welcome.name);
+        overlays.remove(PlayState.gameOver.name);
+        overlays.remove(PlayState.won.name);
+    }
+  }
+
+FutureOr<void> onLoad() async {
     super.onLoad();
 
     camera.viewfinder.anchor = Anchor.topLeft;
-    world.add(PlayArea());
-    world.add(Ball(
-        velocity:
-            Vector2((rand.nextDouble() - 0.5) * width, height * 0.2).normalized()
-              ..scale(height / 4),
-        position: size / 2,
-        radius: ballRadius));
 
-        debugMode = true;
+    world.add(PlayArea());
+
+    playState = PlayState.welcome;                              
   }
+
+  void startGame() {
+    if (playState == PlayState.playing) return;
+
+    world.removeAll(world.children.query<Ball>());
+    world.removeAll(world.children.query<Bat>());
+    world.removeAll(world.children.query<Brick>());
+
+    playState = PlayState.playing;                             
+
+    world.add(Ball(
+        difficultyModifier: difficultyModifier,
+        radius: ballRadius,
+        position: size / 2,
+        velocity: Vector2((rand.nextDouble() - 0.5) * width, height * 0.2)
+            .normalized()
+          ..scale(height / 4)));
+
+    world.add(Bat(
+        size: Vector2(batWidht, batHeight),
+        cornerRadius: const Radius.circular(ballRadius / 2),
+        position: Vector2(width / 2, height * 0.95)));
+
+    world.addAll([                            // Drop the await
+      for (var i = 0; i < brickColors.length; i++)
+        for (var j = 1; j <= 5; j++)
+          Brick(
+            Vector2(
+              (i + 0.5) * brickWidth + (i + 1) * brickGutter,
+              (j + 2.0) * brickHeight + j * brickGutter,
+            ),
+            brickColors[i],
+          ),
+    ]);
+  }                                                             // Drop the debugMode
+
+  @override                                                     // Add from here...
+  void onTap() {
+    super.onTap();
+    startGame();
+  }                   
+
+  KeyEventResult onKeyEvent(
+      RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    super.onKeyEvent(event, keysPressed);
+    switch (event.logicalKey) {
+      case LogicalKeyboardKey.arrowLeft:
+        world.children.query<Bat>().first.moveBy(-batStep);
+      case LogicalKeyboardKey.arrowRight:
+        world.children.query<Bat>().first.moveBy(batStep);
+      case LogicalKeyboardKey.space:                            
+      case LogicalKeyboardKey.enter:
+        startGame();  
+    }
+    return KeyEventResult.handled;
+  }
+  @override
+  Color backgroundColor() => const Color(0xfff2e8cf);       
 }
